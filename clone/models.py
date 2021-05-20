0ad4121f4f4ddd79aa8eb971907372cbcfe49762
Joy Kirii <joykirii@gmail.com>
2021-05-20 17:19:40 +0300

    create image model

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  image = CloudinaryField('image')
+  name = models.CharField(max_length = 30)
+  caption = models.TextField(blank= True)
+  post_date = models.DateTimeField(auto_now_add=True)
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  likes = models.ManyToManyField(Profile, related_name="posts")
+    
+  def __str__(self):
+    return self.name
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
