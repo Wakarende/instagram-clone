3491fcf82b504cae1fe3ff070cd8b384719745aa
Joy Kirii <joykirii@gmail.com>
2021-05-21 22:26:37 +0300

    create upload image and display image views

--- clone/models.py
+++ clone/models.py
@@ -24,7 +24,7 @@ class Image(models.Model):
   caption = models.TextField(blank=True)
   post_date = models.DateTimeField(auto_now_add=True)
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-  # likes = models.ManyToManyField(Profile, related_name="posts")
+  likes = models.ManyToManyField(Profile, related_name="posts")
 
   def __str__(self):
     return self.name
--- clone/views.py
+++ clone/views.py
@@ -6,7 +6,42 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/accounts/login/')
 def home(request):
-  return render(request, 'home.html')
+  title= "Instagram"
+  current_user =request.user
+
+  timeline_images = []
+  current_images = Image.objects.filter(profile = logged_in)
+  for current_image in current_images:
+    timeline_images.append(current_image.id)
+  
+  display_images= Image.objects.filter(pk__in = timeline_images).order_by('-post_date')
+  liked = False
+  for i in display_images:
+    image = Image.objects.get(pk=i.id)
+    liked = False
+    if image.likes.filter(id =request.user.id).exists():
+      liked = True
+
+  return render(request, 'home.html', {"images":display_images,"liked":liked, "comments":comments, "title":title, "loggedIn":logged_in} )
+
+@login_required(login_url='/accounts/login/')
+def upload_image(request):
+  title = "Instagram | Upload image"
+  current_user = request.user
+  try:
+    profile = Profile.objects.get(user = current_user)
+  except Profile.DoesNotExist:
+    raise Http404()
+  if request.method == "POST":
+    form = UploadImageForm(request.POST, request.FILES)
+  if form.is_valid():
+    image = form.save(commit=False)
+    image.profile = profile
+    image.save()
+    return redirect('home')
+  else:
+    form = UploadImageForm()
+  return render(request, 'upload_image.html', {"form": form, "title": title})
 
 # def create_profile(request):
 #   current_user = request.user
