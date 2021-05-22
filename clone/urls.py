537846dffd85428ff1432d4a4277e96939870878
Joy Kirii <joykirii@gmail.com>
2021-05-22 17:44:44 +0300

    create like comment and edit profile url

--- clone/urls.py
+++ clone/urls.py
@@ -8,5 +8,7 @@ path(r'',views.home,name = 'home'),
 path('upload/image', views.upload_image, name = "upload_image"),
 path('create_profile/',views.create_profile,name = 'create_profile'),
 url(r'^profile/(?P<profile_id>\d+)',views.profile,name = 'profile'),
-url(r'^upload/image$', views.upload_image, name = "upload_image"),
+url(r'^like/(?P<image_id>\d+)', views.like_image, name = 'like_image'),
+url(r'^comment/(?P<image_id>\d+)', views.comment,name = "comment"),
+url(r'^profile/edit$', views.profile_edit,name = 'profile_edit'),
 ]
