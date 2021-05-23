ff050332f2bdbff75a36ce1fe530befc397ab738
Joy Kirii <joykirii@gmail.com>
2021-05-23 16:36:07 +0300

    setup comment model test

--- clone/tests.py
+++ clone/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Profile
+from .models import Profile, Image, Comment
 from django.contrib.auth.models import User
 
 
@@ -24,3 +24,22 @@ class TestProfile(TestCase):
   def test_search_user(self):
     user=Profile.search_user(self.user)
     self.assertEqual(len(user),1)
+
+class TestComment(TestCase):
+  def setUp(self):
+    self.user= User(username='joy', email='joy@gmail.com', password='1234')
+    self.profile = Profile(bio='Test profile', user=self.user)
+    self.image= Image(image='default.png', name='test', caption='caption', profile=self.profile)
+    self.comment= Comment(image=self.image, content='comment', user=self.user)
+
+    self.user.save()
+    self.profile.save()
+    self.image.save_image()
+    self.comment.save_comment()
+  
+  def tearDown(self):
+    Image.objects.all().delete()
+    Comment.objects.all().delete()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.comment, Comment))
