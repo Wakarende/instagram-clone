2f0d5ed4a72181bd3547914d8f84b68078dde91f
Joy Kirii <joykirii@gmail.com>
2021-05-24 11:47:51 +0300

    additional styling

--- clone/models.py
+++ clone/models.py
@@ -14,19 +14,19 @@ class Profile(models.Model):
   def __str__(self):
     return self.user.username
 
-  # def create_user_profile(sender, instance, created, **kwargs):
-  #   if created:
-  #     Profile.objects.create(user=instance)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
 
-  #   # @receiver(post_save, sender=User)
-  # def save_user_profile(sender, instance, **kwargs):
-  #   instance.profile.save()
+    # @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
-  # def save_profile(self):
-  #   self.user
+  def save_profile(self):
+    self.user
 
-  # def delete_profile(self):
-  #   self.delete()
+  def delete_profile(self):
+    self.delete()
 
   @classmethod
   def search_user(cls,username): 
