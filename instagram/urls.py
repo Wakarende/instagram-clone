e16afdb09f8b95389eaf397390e5956f8330689f
Joy Kirii <joykirii@gmail.com>
2021-05-25 11:23:21 +0300

    remove email function

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include("clone.urls")),
-    path('accounts/register/', RegistrationView.as_view(success_url='/email'),name='django_registration_register'),
+    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(redirect_field_name ='/',success_url = '/'), {"next_page": '/'}),
